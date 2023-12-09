858f633614e9f117632cf79c84e4355c49260eaa
h.nasiri <h.nasiri@rayanhamafza.com>
2023-12-09 12:19:48 +0330

    completing personAPI

---
+++ src/main/java/org/transport/api/PersonAPI.java
@@ -0,0 +1,46 @@
+package org.transport.api;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.transport.common.CommonUtils;
+import org.transport.model.Person;
+import org.transport.service.GenericService;
+
+import java.util.List;
+
+@RestController
+public class PersonAPI {
+    @Autowired
+    private GenericService<Person> service;
+
+    @PostMapping(path = "/api/person/add")
+    public Long addPerson(@RequestBody Person Person, HttpServletRequest request) {
+        Long userId = CommonUtils.getUserId(CommonUtils.getToken(request));
+        service.insert(Person, userId);
+        return Person.getId();
+    }
+
+    @PostMapping(path = "/api/person/edit")
+    public Long editPerson(@RequestBody Person Person, HttpServletRequest request) {
+        Long userId = CommonUtils.getUserId(CommonUtils.getToken(request));
+        service.update(Person, userId);
+        return Person.getId();
+    }
+
+    @PostMapping(path = "/api/person/remove/{id}")
+    public Long removePerson(@PathVariable Long id) {
+        service.delete(id, Person.class);
+        return id;
+    }
+
+    @GetMapping(path = "/api/person/{id}")
+    public Person getPerson(@PathVariable Long id) {
+        return service.findOne(Person.class, id);
+    }
+
+    @GetMapping(path = "/api/Person")
+    public List<Person> listPerson() {
+        return service.findAll(Person.class);
+    }
+}
---
+++ src/main/java/org/transport/model/Person.java
@@ -0,0 +1,47 @@
+package org.transport.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "[person]", schema = "tpt")
+@Entity(name = "person")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "NVARCHAR(50)")
+    private String name;
+    @Column(columnDefinition = "NVARCHAR(50)")
+    private String family;
+    @Column(columnDefinition = "NVARCHAR(10)", name = "birth_date")
+    private String birthDate;
+    @Column(columnDefinition = "NVARCHAR(11)", name = "national_code")
+    private String nationalCode;
+    @Column(columnDefinition = "NVARCHAR(50)", name = "mobile_number")
+    private String mobileNumber;
+    @Column(columnDefinition = "BIT", name = "is_foreigners")
+    private Boolean isForeigners;
+    @Column(columnDefinition = "BIT", name = "is_company")
+    private Boolean isCompany;
+    @Column(columnDefinition = "NVARCHAR(50)", name = "company_national_code")
+    private String companyNationalCode;
+    @Column(columnDefinition = "NVARCHAR(10)", name = "establish_date")
+    private String establishDate;
+    @Column(columnDefinition = "NVARCHAR(50)", name = "manager_name")
+    private String managerName;
+    @Column(columnDefinition = "NVARCHAR(10)", name = "manager_national_code")
+    private String managerNationalCode;
+    @Column(columnDefinition = "NVARCHAR(10)", name = "manager_birth_date")
+    private String managerBirthDate;
+    @Column(columnDefinition = "NVARCHAR(50)", name = "manager_mobile_number")
+    private String managerMobileNumber;
+    @Column(columnDefinition = "NVARCHAR(50)", name = "id_number")
+    private String idNumber;
+}
